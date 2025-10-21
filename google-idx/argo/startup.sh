8835de21bf2f12d4f8cee0e5ab8a2a4d8f8269ae
kuisa <32757027+kuisa@users.noreply.github.com>
2025-10-21 11:21:31 +0800

    Update startup.sh

--- google-idx/argo/startup.sh
+++ google-idx/argo/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ARGO_TOKEN=
+ARGO_TOKEN="eyJhIjoiZmQ5YjNkMDdkOWQxZWYxY2M4OGY2ZTJiNDE2OTNmZmUiLCJ0IjoiZWI5ZTU4NDgtMGJlNy00ZmIzLWI4ZjUtYzA2NWQzZDg2MTNmIiwicyI6Ik9EUTFNV1ptWmprdFlqazBPQzAwWmpBMkxUa3daak10WmprNVpHRm1PVGN3T0RNdyJ9"
 
 if [ -z "$ARGO_TOKEN" ]; then
   nohup $PWD/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 --url http://localhost:8090 1>$PWD/argo.log 2>&1 &
